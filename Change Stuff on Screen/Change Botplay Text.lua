161db899630e2202040622fcf798ea0bfaca4c72
ProgramerGamerZone <66317267+ProgramerGamerZone@users.noreply.github.com>
2022-03-05 14:26:08 +0400

    Create Change Botplay Text.lua

---
+++ Change Stuff on Screen/Change Botplay Text.lua
@@ -0,0 +1,6 @@
+--By https://github.com/ErenKaya31
+function onUpdate(elapsed)
+    setTextFont('botplayTxt', 'comic.ttf');
+    setTextColor('botplayTxt', '0xFF0000');
+    setTextString('botplayTxt', "CHEATER!");
+end
